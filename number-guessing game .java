9917a3e2fe6261608db7c807bd5b0fa293610241
numanmalik09 <nm693094@gmail.com>
2026-08-02 17:22:28 +0530

    Refactor number guessing game logic and formatting

--- number-guessing game .java
+++ number-guessing game .java
@@ -1,29 +1,29 @@
+
 import java.sql.SQLOutput;
 import java.util.Scanner;
-
-public class Demo {
+public class numberguessinggame {
     public static void main(String[] args) {
-        
-        // NUMBER GUESSING GAME 
-      
+        // NUMBER GUESSING GAME
+
         int random_number;
-         random_number = (int)(Math.random() *100 +1); // Random Numnber GENERATE 
+        random_number = (int) (Math.random() * 100 + 1); // Random Numnber GENERATE
 
-Scanner sc= new Scanner(System.in);      // Scanner Activition 
+        Scanner sc = new Scanner(System.in);      // Scanner Activition
         System.out.println("enter a number between 1-100");
- int usernum;   // user number variable 
-      // Game logic 
-do{
- 
-   usernum = sc.nextInt();
-    if( usernum < random_number) {
-        System.out.println("Too Low : TRY AGAIN !"); // IF USER INPUT IS SMALLER THEN RANDOM NUMBER THIS MESSAFE  PRINTS 
-    }else if ( usernum > random_number) {
-        System.out.println("Too High : TRY AGAIN !"); // IF USER INPUT IS GREATER  THEN RANDOM NUMBER THIS MESSAGE  PRINTS 
+        int usernum;   // user number variable
+        // Game logic
+        do {
 
-    }else
-    System.out.println("Congrats You Won The Game "); //  IF USER INPUT IS EQUALS  THEN RANDOM NUMBER THIS MESSAGE PRINTS 
-     // ENDING THE LOOP HERE 
-}while(usernum !=random_number);
+            usernum = sc.nextInt();
+            if (usernum < random_number) {
+                System.out.println("Too Low : TRY AGAIN !"); // IF USER INPUT IS SMALLER THEN RANDOM NUMBER THIS MESSAFE  PRINTS
+            } else if (usernum > random_number) {
+                System.out.println("Too High : TRY AGAIN !"); // IF USER INPUT IS GREATER  THEN RANDOM NUMBER THIS MESSAGE  PRINTS
 
+            } else
+                System.out.println("Congrats You Won The Game "); //  IF USER INPUT IS EQUALS  THEN RANDOM NUMBER THIS MESSAGE PRINTS
+            // ENDING THE LOOP HERE
+        } while (usernum != random_number);
 
+    }
+}
